4a294fdf34564dd45c890ad827457d500a13b2e4
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-08-09 23:32:10 -0700

    NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Product from "./pages/Product"
+import Home from "./pages/Home"
+import Pricing from "./pages/Pricing"
+import PageNotFound from "./pages/PageNotFound"
 
-function App() {
+const App = () => {
   return (
-		<div>App</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/product" element={<Product />} />
+				<Route path="/pricing" element={<Pricing />} />
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</Router>
 	)
 }
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import PageNav from "../components/PageNav"
+
+
+const Home = () => {
+	return (
+		<div>
+			<PageNav />
+			<h1>TrackOrTrick</h1>
+		</div>
+	)
+}
+
+export default Home
