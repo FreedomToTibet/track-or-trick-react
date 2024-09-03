d7417c315181661b5158419cd0546592c05421f3
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-09-03 16:25:53 -0700

    update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,7 +11,7 @@ const Homepage = () => {
         <h1>
           You travel the world.
           <br />
-          WorldWise keeps track of your adventures.
+          Track-Trick keeps track of your adventures.
         </h1>
         <h2>
           A world map that tracks your footsteps into every city you can think
